b9abfa86011fd2ec1765db2ba16be67f00087b75
AlanCanalesM <alan.canales@micorreo.upp.edu.mx>
2023-10-04 22:01:58 -0600

    Adds axios to request the go backend service

--- src/components/ArticleBoxList/ArticleBoxList.jsx
+++ src/components/ArticleBoxList/ArticleBoxList.jsx
@@ -1,52 +1,65 @@
 import ArticleBox from "../ArticleBox/ArticleBox"
 import ArticleBoxListStyle from "./ArticleBoxList.module.css"
+import { useState } from "react"
+import axios from 'axios'
+
+
 const mockData = [
     {
-        title: 'Tentative Deal Reached to keep government open without full wall funding',
-        readed: false
+        "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "feed_id": 1
     },
     {
-        title: 'A woman just not a woman in the world of sports',
-        readed: false
+        "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "feed_id": 2
     },
     {
-        title: 'When social media goes too far, whats next?',
-        readed: false
+        "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "feed_id": 1
     },
     {
-        title: 'Tentative Deal Reached to keep government open without full wall funding',
-        readed: false
+        "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "feed_id": 2
     },
     {
-        title: 'A woman just not a woman in the world of sports',
-        readed: false
+        "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "feed_id": 1
     },
     {
-        title: 'When social media goes too far, whats next?',
-        readed: false
-    }, {
-        title: 'Tentative Deal Reached to keep government open without full wall funding',
-        readed: false
+        "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "feed_id": 2
     },
     {
-        title: 'A woman just not a woman in the world of sports',
-        readed: false
+        "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "feed_id": 1
     },
     {
-        title: 'When social media goes too far, whats next?',
-        readed: false
+        "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "feed_id": 2
     }
 ]
 
-
 function ArticleBoxList() {
+
+    // const [posts, setPosts] = useState([]);
+
+    // axios.get('http://localhost:8080/v1/allPosts')
+    // .then(response => {
+    //     setPosts(response.data)
+    // })
+    // .catch(error => {
+    //     console.log(error)
+    // }
+    // )
+
+    
     return (
         <div className={ArticleBoxListStyle.list}>
             {mockData.map((data, index) => (
                 <ArticleBox
                     key={index} // Use a unique key for each item when mapping
                     title={data.title}
-                    readed={data.readed}
+                    readed={data.feed_id}
                 />
             ))}
         </div>
